09a66b52b21b6cabc2e37afd460dc7ad3718d178
asif <muhammadasifashraf270@gmail.com>
2022-10-17 11:01:22 +0400

    First Problem Data Structure and Algorithums

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> input=new ArrayList<Integer>();
+        input.add(1);
+        input.add(2);
+        input.add(3);
+        input.add(4);
+        List output=FirstProblem.reverseArray(input);
+        System.out.println(output.toString());
+
     }
 }
